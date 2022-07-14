f5e8634b10deeea737868d51c4dc8d4eab3efb6a
ZHAO ZHANG <z5355933@ad.unsw.edu.au>
2022-07-14 22:11:46 +0800

    update

--- src/test/java/dungeonmania/CollectAndBuildTest.java
+++ src/test/java/dungeonmania/CollectAndBuildTest.java
@@ -28,14 +28,13 @@ import dungeonmania.util.Position;
 public class CollectAndBuildTest {
     
     @Test
-    @DisplayName("Test the player can pick all CollectableEntity in map")
-    public void testMovementDown() {
+    @DisplayName("Test the player can pick up wood in map")
+    public void testPickUpWood() {
 
         DungeonManiaController dmc = new DungeonManiaController();
 
         DungeonResponse res = dmc.newGame("d_collectTests_pickUpAllCollectableEntity",
                 "c_collectTests");
-
         // pick up wood
         res = dmc.tick(Direction.RIGHT);
         assertEquals(1, getInventory(res, "wood").size());
@@ -43,12 +42,47 @@ public class CollectAndBuildTest {
         //pick up wood again
         res = dmc.tick(Direction.RIGHT);
         assertEquals(2, getInventory(res, "wood").size());
+    }
+
+
+    @Test
+    @DisplayName("Test the player can pick arrow in map")
+    public void testPickUpArrow() {
+
+        DungeonManiaController dmc = new DungeonManiaController();
+
+        DungeonResponse res = dmc.newGame("d_collectTests_pickUpAllCollectableEntity",
+                "c_collectTests");
 
         //pick up arrow
         res = dmc.tick(Direction.RIGHT);
+        res = dmc.tick(Direction.RIGHT);
+        res = dmc.tick(Direction.RIGHT);
         assertEquals(2, getInventory(res, "wood").size());
         assertEquals(1, getInventory(res, "arrow").size());
-    }
 
+        //pick up wood again
+        res = dmc.tick(Direction.RIGHT);
+        assertEquals(2, getInventory(res, "wood").size());
+        assertEquals(2, getInventory(res, "arrow").size());
+    }
+    
+
+    @Test
+    @DisplayName("Test the player can pick invincibility_potion in map")
+    public void testPickUpInvincibilityPotion() {
+
+        DungeonManiaController dmc = new DungeonManiaController();
+
+        DungeonResponse res = dmc.newGame("d_collectTests_pickUpAllCollectableEntity",
+                "c_collectTests");
+
+        res = dmc.tick(Direction.RIGHT);
+        res = dmc.tick(Direction.RIGHT);
+        res = dmc.tick(Direction.RIGHT);
+        res = dmc.tick(Direction.RIGHT);
+        res = dmc.tick(Direction.RIGHT);
+        assertEquals(1, getInventory(res, "invincibility_potion").size());
+    }
 
 }
