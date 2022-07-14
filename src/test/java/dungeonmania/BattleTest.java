b627892f3d61f9245097da4d359c5e4a965b876b
Shilong li <li77534479@gmail.com>
2022-07-14 12:55:18 +1000

    add feacture for map generator, print map

--- src/test/java/dungeonmania/BattleTest.java
+++ src/test/java/dungeonmania/BattleTest.java
@@ -25,19 +25,19 @@ import dungeonmania.response.models.RoundResponse;
 import dungeonmania.util.Direction;
 import dungeonmania.util.Position;
 public class BattleTest {
-        private static DungeonResponse genericMercenarySequence(DungeonManiaController controller, String configFile) {
-            /*
-            * exit wall wall wall
-            * player [ ] merc wall
-            * wall wall wall wall
-            */
-            DungeonResponse initialResponse = controller.newGame("d_battleTest_basicMercenary", configFile);
-            int mercenaryCount = countEntityOfType(initialResponse, "mercenary");
+        // private static DungeonResponse genericMercenarySequence(DungeonManiaController controller, String configFile) {
+        //     /*
+        //     * exit wall wall wall
+        //     * player [ ] merc wall
+        //     * wall wall wall wall
+        //     */
+        //     DungeonResponse initialResponse = controller.newGame("d_battleTest_basicMercenary", configFile);
+        //     int mercenaryCount = countEntityOfType(initialResponse, "mercenary");
 
-            assertEquals(1, countEntityOfType(initialResponse, "player"));
-            assertEquals(1, mercenaryCount);
-            return controller.tick(Direction.RIGHT);
-        }
+        //     assertEquals(1, countEntityOfType(initialResponse, "player"));
+        //     assertEquals(1, mercenaryCount);
+        //     return controller.tick(Direction.RIGHT);
+        // }
 
         private void assertBattleCalculations(String enemyType, BattleResponse battle, boolean enemyDies,
                 String configFilePath) {
@@ -63,127 +63,130 @@ public class BattleTest {
     @Test
     public void testBattleWithSpiderWin() {
         DungeonManiaController controller = new DungeonManiaController();
-        DungeonResponse postBattleResponse = genericMercenarySequence(controller,
-                "c_battleTests_basicMercenaryPlayerDies");
+        DungeonResponse initialResponse = controller.newGame("d_battleTest_basicMercenary", "c_battleTests_basicMercenaryPlayerDies");
+        DungeonResponse postBattleResponse = controller.tick(Direction.RIGHT);
+        BattleResponse battle = postBattleResponse.getBattles().get(0);
+
+        assertBattleCalculations("spider",battle, true,"c_battleTests_basicMercenaryPlayerDies")
     }
     @Test
     public void testBattleWithSpiderLost() {
         DungeonManiaController controller = new DungeonManiaController();
-        DungeonResponse postBattleResponse = genericMercenarySequence(controller,
-                "c_battleTests_basicMercenaryPlayerDies");
+        // DungeonResponse postBattleResponse = genericMercenarySequence(controller,
+        //         "c_battleTests_basicMercenaryPlayerDies");
     }
     @Test
     public void testBattleWithZombieWin() {
         DungeonManiaController controller = new DungeonManiaController();
-        DungeonResponse postBattleResponse = genericMercenarySequence(controller,
-                "c_battleTests_basicMercenaryPlayerDies");
+        // DungeonResponse postBattleResponse = genericMercenarySequence(controller,
+        //         "c_battleTests_basicMercenaryPlayerDies");
     }
     @Test
     public void testBattleWithZombieLost() {
         DungeonManiaController controller = new DungeonManiaController();
-        DungeonResponse postBattleResponse = genericMercenarySequence(controller,
-                "c_battleTests_basicMercenaryPlayerDies");
+        // DungeonResponse postBattleResponse = genericMercenarySequence(controller,
+        //         "c_battleTests_basicMercenaryPlayerDies");
     }
     @Test
     public void testBattleWithMerceariesWin() {
         DungeonManiaController controller = new DungeonManiaController();
-        DungeonResponse postBattleResponse = genericMercenarySequence(controller,
-                "c_battleTests_basicMercenaryPlayerDies");
+        // DungeonResponse postBattleResponse = genericMercenarySequence(controller,
+        //         "c_battleTests_basicMercenaryPlayerDies");
     }
     @Test
     public void testBattleWithMerceariesLost() {
         DungeonManiaController controller = new DungeonManiaController();
-        DungeonResponse postBattleResponse = genericMercenarySequence(controller,
-                "c_battleTests_basicMercenaryPlayerDies");
+        // DungeonResponse postBattleResponse = genericMercenarySequence(controller,
+        //         "c_battleTests_basicMercenaryPlayerDies");
     }
     @Test
     public void testBattleWithEnemyInvincibilityPotionWin() {
         DungeonManiaController controller = new DungeonManiaController();
-        DungeonResponse postBattleResponse = genericMercenarySequence(controller,
-                "c_battleTests_basicMercenaryPlayerDies");
+        // DungeonResponse postBattleResponse = genericMercenarySequence(controller,
+        //         "c_battleTests_basicMercenaryPlayerDies");
     }
     @Test
     public void testBattleWithEnemyInvincibilityPotionLost() {
         DungeonManiaController controller = new DungeonManiaController();
-        DungeonResponse postBattleResponse = genericMercenarySequence(controller,
-                "c_battleTests_basicMercenaryPlayerDies");
+        // DungeonResponse postBattleResponse = genericMercenarySequence(controller,
+        //         "c_battleTests_basicMercenaryPlayerDies");
     }
     @Test
     public void testBattleWithEnemyInvisibilityPotionWin() {
         DungeonManiaController controller = new DungeonManiaController();
-        DungeonResponse postBattleResponse = genericMercenarySequence(controller,
-                "c_battleTests_basicMercenaryPlayerDies");
+        // DungeonResponse postBattleResponse = genericMercenarySequence(controller,
+        //         "c_battleTests_basicMercenaryPlayerDies");
     }
     @Test
     public void testBattleWithEnemyInvisibilityPotionLost() {
         DungeonManiaController controller = new DungeonManiaController();
-        DungeonResponse postBattleResponse = genericMercenarySequence(controller,
-                "c_battleTests_basicMercenaryPlayerDies");
+        // DungeonResponse postBattleResponse = genericMercenarySequence(controller,
+        //         "c_battleTests_basicMercenaryPlayerDies");
     }
     @Test
     public void testBattleWithEnemySwordWin() {
         DungeonManiaController controller = new DungeonManiaController();
-        DungeonResponse postBattleResponse = genericMercenarySequence(controller,
-                "c_battleTests_basicMercenaryPlayerDies");
+        // DungeonResponse postBattleResponse = genericMercenarySequence(controller,
+        //         "c_battleTests_basicMercenaryPlayerDies");
     }
     @Test
     public void testBattleWithEnemySwordLost() {
         DungeonManiaController controller = new DungeonManiaController();
-        DungeonResponse postBattleResponse = genericMercenarySequence(controller,
-                "c_battleTests_basicMercenaryPlayerDies");
+        // DungeonResponse postBattleResponse = genericMercenarySequence(controller,
+        //         "c_battleTests_basicMercenaryPlayerDies");
     }
     @Test
     public void testBattleWithEnemyBowWin() {
         DungeonManiaController controller = new DungeonManiaController();
-        DungeonResponse postBattleResponse = genericMercenarySequence(controller,
-                "c_battleTests_basicMercenaryPlayerDies");
+        // DungeonResponse postBattleResponse = genericMercenarySequence(controller,
+        //         "c_battleTests_basicMercenaryPlayerDies");
     }
     @Test
     public void testBattleWithEnemyBowLost() {
         DungeonManiaController controller = new DungeonManiaController();
-        DungeonResponse postBattleResponse = genericMercenarySequence(controller,
-                "c_battleTests_basicMercenaryPlayerDies");
+        // DungeonResponse postBattleResponse = genericMercenarySequence(controller,
+        //         "c_battleTests_basicMercenaryPlayerDies");
     }
     @Test
     public void testBattleWithEnemyShieldWin() {
         DungeonManiaController controller = new DungeonManiaController();
-        DungeonResponse postBattleResponse = genericMercenarySequence(controller,
-                "c_battleTests_basicMercenaryPlayerDies");
+        // DungeonResponse postBattleResponse = genericMercenarySequence(controller,
+        //         "c_battleTests_basicMercenaryPlayerDies");
     }
     @Test
     public void testBattleWithEnemyShieldGtEnemyAttack() {
         DungeonManiaController controller = new DungeonManiaController();
-        DungeonResponse postBattleResponse = genericMercenarySequence(controller,
-                "c_battleTests_basicMercenaryPlayerDies");
+        // DungeonResponse postBattleResponse = genericMercenarySequence(controller,
+        //         "c_battleTests_basicMercenaryPlayerDies");
     }
     @Test
     public void testBattleWithEnemyShieldLost() {
         DungeonManiaController controller = new DungeonManiaController();
-        DungeonResponse postBattleResponse = genericMercenarySequence(controller,
-                "c_battleTests_basicMercenaryPlayerDies");
+        // DungeonResponse postBattleResponse = genericMercenarySequence(controller,
+        //         "c_battleTests_basicMercenaryPlayerDies");
     }
     @Test
     public void testBattleWithEnemySwordAndBowWin() {
         DungeonManiaController controller = new DungeonManiaController();
-        DungeonResponse postBattleResponse = genericMercenarySequence(controller,
-                "c_battleTests_basicMercenaryPlayerDies");
+        // DungeonResponse postBattleResponse = genericMercenarySequence(controller,
+        //         "c_battleTests_basicMercenaryPlayerDies");
     }
     @Test
     public void testBattleWithEnemySwordAndBowLost() {
                 DungeonManiaController controller = new DungeonManiaController();
-        DungeonResponse postBattleResponse = genericMercenarySequence(controller,
-                "c_battleTests_basicMercenaryPlayerDies");
+        // DungeonResponse postBattleResponse = genericMercenarySequence(controller,
+        //         "c_battleTests_basicMercenaryPlayerDies");
     }
     @Test
     public void testBattleWithEnemySwordAndBowAndShieldWin() {
         DungeonManiaController controller = new DungeonManiaController();
-        DungeonResponse postBattleResponse = genericMercenarySequence(controller,
-                "c_battleTests_basicMercenaryPlayerDies");
+        // DungeonResponse postBattleResponse = genericMercenarySequence(controller,
+        //         "c_battleTests_basicMercenaryPlayerDies");
     }
     @Test
     public void testBattleWithEnemySwordAndBowAndShieldLost() {
                 DungeonManiaController controller = new DungeonManiaController();
-        DungeonResponse postBattleResponse = genericMercenarySequence(controller,
-                "c_battleTests_basicMercenaryPlayerDies");
+        // DungeonResponse postBattleResponse = genericMercenarySequence(controller,
+        //         "c_battleTests_basicMercenaryPlayerDies");
     }
 }
