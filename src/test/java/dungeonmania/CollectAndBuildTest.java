a63dfcc8dd3b83f64404849f7e5b7dd5eaa705bd
ZHAO ZHANG <z5355933@ad.unsw.edu.au>
2022-07-14 21:51:18 +0800

    update

---
+++ src/test/java/dungeonmania/CollectAndBuildTest.java
@@ -0,0 +1,54 @@
+package dungeonmania;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import static dungeonmania.TestUtils.getPlayer;
+import static dungeonmania.TestUtils.getEntities;
+import static dungeonmania.TestUtils.getInventory;
+import static dungeonmania.TestUtils.getGoals;
+import static dungeonmania.TestUtils.countEntityOfType;
+import static dungeonmania.TestUtils.getValueFromConfigFile;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import dungeonmania.response.models.BattleResponse;
+import dungeonmania.response.models.DungeonResponse;
+import dungeonmania.response.models.EntityResponse;
+import dungeonmania.response.models.RoundResponse;
+import dungeonmania.util.Direction;
+import dungeonmania.util.Position;
+
+public class CollectAndBuildTest {
+    
+    @Test
+    @DisplayName("Test the player can pick all CollectableEntity in map")
+    public void testMovementDown() {
+
+        DungeonManiaController dmc = new DungeonManiaController();
+
+        DungeonResponse res = dmc.newGame("d_collectTests_pickUpAllCollectableEntity",
+                "c_collectTests");
+
+        // pick up wood
+        res = dmc.tick(Direction.RIGHT);
+        assertEquals(1, getInventory(res, "wood").size());
+
+        //pick up wood again
+        res = dmc.tick(Direction.RIGHT);
+        assertEquals(2, getInventory(res, "wood").size());
+
+        //pick up arrow
+        res = dmc.tick(Direction.RIGHT);
+        assertEquals(2, getInventory(res, "wood").size());
+        assertEquals(1, getInventory(res, "arrow").size());
+    }
+
+
+}
