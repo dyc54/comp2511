803cc2635ad11e9e91ecff40cbf58afd067feaf0
Shilong li <li77534479@gmail.com>
2022-07-25 17:40:26 +1000

    adding more comperhensive test

--- src/test/java/dungeonmania/testGenerateDungeon.java
+++ src/test/java/dungeonmania/testGenerateDungeon.java
@@ -33,4 +33,48 @@ public class testGenerateDungeon {
         assertEquals(1, getEntities(res, "exit").size());
         assertEquals(1, getEntities(res, "player").size());
     }
+    @Test
+    public void testPositions() {
+        DungeonManiaController controller = new DungeonManiaController();
+        DungeonResponse res =  controller.generateDungeon(0, 0, 15, 15, "c_Battletest_PlayerStrong");
+        assertEquals(1, getEntities(res, "exit").size());
+        assertEquals(1, getEntities(res, "player").size());
+        assertEquals(new Position(0, 0), getEntities(res, "player").get(0).getPosition());
+        assertEquals(new Position(15, 15), getEntities(res, "exit").get(0).getPosition());
+    }
+    @Test
+    public void testEdgeCaseLarge() {
+        DungeonManiaController controller = new DungeonManiaController();
+        DungeonResponse res =  controller.generateDungeon(0, 0, 1, 10, "c_Battletest_PlayerStrong");
+        assertEquals(1, getEntities(res, "exit").size());
+        assertEquals(1, getEntities(res, "player").size());
+        assertEquals(new Position(0, 0), getEntities(res, "player").get(0).getPosition());
+        assertEquals(new Position(1, 10), getEntities(res, "exit").get(0).getPosition());
+
+    }
+    @Test
+    public void testEdgeCaseSmall() {
+        DungeonManiaController controller = new DungeonManiaController();
+        DungeonResponse res =  controller.generateDungeon(0, 0, 1, 1, "c_Battletest_PlayerStrong");
+        assertEquals(1, getEntities(res, "exit").size());
+        assertEquals(1, getEntities(res, "player").size());
+        assertEquals(new Position(0, 0), getEntities(res, "player").get(0).getPosition());
+        assertEquals(new Position(1, 1), getEntities(res, "exit").get(0).getPosition());
+
+    }
+    @Test
+    public void testPlayerMovement() {
+        DungeonManiaController controller = new DungeonManiaController();
+        DungeonResponse res =  controller.generateDungeon(0, 0, 1, 1, "c_Battletest_PlayerStrong");
+        assertDoesNotThrow(() -> {
+            controller.tick(Direction.DOWN);
+        });
+    }
+    @Test
+    public void testGoal() {
+        DungeonManiaController controller = new DungeonManiaController();
+        DungeonResponse res =  controller.generateDungeon(0, 0, 1, 1, "c_Battletest_PlayerStrong");
+        assertTrue(res.getGoals().contains(":exit"));
+        
+    }
 }
