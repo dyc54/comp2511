015ad8923f70c233b04f1c32da119a14ca302bdc
Shilong li <li77534479@gmail.com>
2022-07-27 14:04:51 +1000

    to be compatible with m2 config files

--- src/main/java/dungeonmania/helpers/Config.java
+++ src/main/java/dungeonmania/helpers/Config.java
@@ -72,19 +72,62 @@ public class Config {
         zombie_attack = json.getInt("zombie_attack");
         zombie_health = json.getInt("zombie_health");
         zombie_spawn_rat = json.getInt("zombie_spawn_rate");
+        int assassin_attack;
+        int assassin_bribe_amount;
+        int assassin_bribe_fail_rate;
+        int assassin_health;
+        int assassin_recon_radius;
+        int hydra_spawn_rate;
+        int hydra_attack;
+        int hydra_health;
+        int hydra_health_increase_rate;
+        int hydra_health_increase_amount;
+        int mind_control_duration;
+        int midnight_armour_attack;
+        int midnight_armour_defence;
 
-        assassin_attack = json.getInt("assassin_attack");
-        assassin_bribe_amount = json.getInt("assassin_bribe_amount");
-        assassin_bribe_fail_rate = json.getInt("assassin_bribe_fail_rate");
-        assassin_health = json.getInt("assassin_health");
-        assassin_recon_radius = json.getInt("assassin_recon_radius");
-        hydra_spawn_rate = json.getInt("hydra_spawn_rate");
-        hydra_attack = json.getInt("hydra_attack");
-        hydra_health = json.getInt("hydra_health");
-        hydra_health_increase_rate = json.getInt("hydra_health_increase_rate");
-        hydra_health_increase_amount = json.getInt("hydra_health_increase_amount");
-        mind_control_duration = json.getInt("mind_control_duration");
-        midnight_armour_attack = json.getInt("midnight_armour_attack");
-        midnight_armour_defence = json.getInt("midnight_armour_defence");
+        // * to be compatible with m2 config files.
+        try {
+            assassin_attack = json.getInt("assassin_attack");
+            assassin_bribe_amount = json.getInt("assassin_bribe_amount");
+            assassin_bribe_fail_rate = json.getInt("assassin_bribe_fail_rate");
+            assassin_health = json.getInt("assassin_health");
+            assassin_recon_radius = json.getInt("assassin_recon_radius");
+            hydra_spawn_rate = json.getInt("hydra_spawn_rate");
+            hydra_attack = json.getInt("hydra_attack");
+            hydra_health = json.getInt("hydra_health");
+            hydra_health_increase_rate = json.getInt("hydra_health_increase_rate");
+            hydra_health_increase_amount = json.getInt("hydra_health_increase_amount");
+            mind_control_duration = json.getInt("mind_control_duration");
+            midnight_armour_attack = json.getInt("midnight_armour_attack");
+            midnight_armour_defence = json.getInt("midnight_armour_defence");
+        } catch (JSONException exp) {
+            assassin_attack = 0;
+            assassin_bribe_amount = 0;
+            assassin_bribe_fail_rate = 0;
+            assassin_health = 0;
+            assassin_recon_radius = 0;
+            hydra_spawn_rate = 0;
+            hydra_attack = 0;
+            hydra_health = 0;
+            hydra_health_increase_rate = 0;
+            hydra_health_increase_amount = 0;
+            mind_control_duration = 0;
+            midnight_armour_attack = 0;
+            midnight_armour_defence = 0;
+        }
+        this.assassin_attack = assassin_attack;
+        this.assassin_bribe_amount = assassin_bribe_amount;
+        this.assassin_bribe_fail_rate = assassin_bribe_fail_rate;
+        this.assassin_health = assassin_health;
+        this.assassin_recon_radius = assassin_recon_radius;
+        this.hydra_spawn_rate = hydra_spawn_rate;
+        this.hydra_attack = hydra_attack;
+        this.hydra_health = hydra_health;
+        this.hydra_health_increase_rate = hydra_health_increase_rate;
+        this.hydra_health_increase_amount = hydra_health_increase_amount;
+        this.mind_control_duration = mind_control_duration;
+        this.midnight_armour_attack = midnight_armour_attack;
+        this.midnight_armour_defence = midnight_armour_defence;
     }
 }
