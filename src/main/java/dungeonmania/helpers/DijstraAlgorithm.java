83886cfbf68bd23a53b42d0248232f2f75646e76
ZHAO ZHANG <z5355933@ad.unsw.edu.au>
2022-07-28 02:10:18 +0800

    DijstraAlgorithm

--- src/main/java/dungeonmania/helpers/DijstraAlgorithm.java
+++ src/main/java/dungeonmania/helpers/DijstraAlgorithm.java
@@ -43,13 +43,19 @@ import dungeonmania.staticEntities.Wall;
 public class DijstraAlgorithm {
 
     private static int MaxValue = 100000;
-    private int vertex;
     private Location destination;
     private Location next;
     private Location source;
     private Collection<Entity> entities;
     private DijstraPosition[][] maze;
     private TreeMap<Location, HashSet<Entity>> map;
+    private int sourceX;
+    private int sourceY;
+    private int hight;
+    private int width;
+
+    private int destinationX ;
+    private int destinationY;
 
     private HashMap<DijstraPosition, Integer> dist = new HashMap<>();
 
@@ -58,22 +64,40 @@ public class DijstraAlgorithm {
         source = enemy;
         map = dungeonMap.gMap();
         entities = dungeonMap.getAllEntities();
+        sourceX  = getMazeX(source.getX());
+        sourceY  = getMazeY(source.getY());
+    
+        destinationX  = getMazeX(destination.getX());
+        destinationY  = getMazeX(destination.getY());
+    }
+
+    private int getMazeX(int x) {
+       if(x - map.firstKey().getY() < 0){
+         return Math.abs(x - map.firstKey().getY());
+       }else{
+        return x - map.firstKey().getY();
+       }
+    }
+
+    private int getMazeY(int y) {
+        if(y - map.firstKey().getX() < 0){
+            return Math.abs(y - map.firstKey().getX());
+          }else{
+            return y - map.firstKey().getX();
+           }
     }
 
     public void initializationMaze(){
 
         RandomMapGenerator HW = new RandomMapGenerator(map.firstKey().getX(), map.firstKey().getY(), map.lastKey().getX(), map.lastKey().getY());
-        maze = new DijstraPosition[HW.getWidth()][HW.getHeight()];
+        hight = HW.getHeight()+1;
+        width = HW.getWidth()+1;
+        maze = new DijstraPosition[width][hight];
         
         /* Initialize start and end points */
-        int sourcex  = source.getX()- map.firstKey().getY();
-        int sourcey  = source.getY() - map.firstKey().getX();
 
-        int destinationx  = destination.getX()- map.firstKey().getY();
-        int destinationy  = destination.getY() - map.firstKey().getX();
-
-        maze[sourcex][sourcey] = new DijstraPosition(source.getX(),source.getY(),1,false,sourcex,sourcey);
-        maze[destinationx][destinationy] = new DijstraPosition(destination.getX(),destination.getY(),1,true,destinationx,destinationy);
+        maze[sourceX][sourceY] = new DijstraPosition(source.getX(),source.getY(),1,false,sourceX,sourceY);
+        maze[destinationX][destinationY] = new DijstraPosition(destination.getX(),destination.getY(),1,true,destinationX ,destinationY);
 
         /* Initialize walls and boulders  and door */
         for(Entity entity : entities){
@@ -119,12 +143,12 @@ public class DijstraAlgorithm {
         }
 
         /* Initialize distance */
-        for (int i = 0; i < vertex; i++) {
-            for (int j = 0; j < vertex; j++) {
+        for (int i = 0; i < HW.getWidth(); i++) {
+            for (int j = 0; j < HW.getHeight(); j++) {
                 dist.put(maze[i][j], MaxValue);
             }
         }
-        dist.replace( maze[source.getX()][source.getY()], 0);
+        dist.replace( maze[sourceX][sourceY], 0);
     }
 
     public Location dijstra(){
@@ -134,23 +158,25 @@ public class DijstraAlgorithm {
         int[][] operate = { { 0, -1 }, { 0, 1 }, { -1, 0 }, { 1, 0 } };
         Queue<DijstraPosition> wait = new LinkedList<DijstraPosition>();
         
-        wait.add(maze[source.getX()][source.getY()]);
+        wait.add(maze[sourceX][sourceY]);
         while (!wait.isEmpty()) {
             DijstraPosition cur = wait.poll();
             /* outPut(cur.x, cur.y); */
-            if (cur.x == destination.getX() && cur.y == destination.getY()) {
-                outPut(destination.getX(), destination.getY());
+            if (cur.x == destinationX  && cur.y == destinationY) {
+                outPut(destinationX, destinationY);
                 break;
             }
             cur.reachable = false;
             for (int i = 0; i < 4; i++) {
                 int nextX = cur.x + operate[i][0];
                 int nextY = cur.y + operate[i][1];
-                int cost = maze[nextX][nextY].cost + cur.cost;
-                if (maze[nextX][nextY].reachable && dist.get(cur) + cost < dist.get(maze[nextX][nextY])) {
-                    dist.replace(maze[nextX][nextY], dist.get(cur) + cost);
-                    wait.add(maze[nextX][nextY]);
-                    maze[nextX][nextY].pre = cur;
+                if(nextX < width && nextY < hight && nextX >= 0 && nextY >= 0){
+                    int cost = maze[nextX][nextY].cost + cur.cost;
+                    if (maze[nextX][nextY].reachable && dist.get(cur) + cost < dist.get(maze[nextX][nextY])) {
+                        dist.replace(maze[nextX][nextY], dist.get(cur) + cost);
+                        wait.add(maze[nextX][nextY]);
+                        maze[nextX][nextY].pre = cur;
+                    }
                 }
             }
         }
@@ -158,16 +184,6 @@ public class DijstraAlgorithm {
     }
 
     public void outPut(int x, int y) {
-        if(maze[x][y].pre == null){
-            return;
-        }
-        if (maze[x][y].pre.pre == null) {
-            next = new Location(x, y);
-        }
-        outPut(maze[x][y].pre.x, maze[x][y].pre.y);
-    }
-
-    public void get(int x, int y) {
         if(maze[x][y].pre == null){
             return;
         }
@@ -187,8 +203,6 @@ class DijstraPosition {
     int cost;
     boolean reachable;
     DijstraPosition pre;
-
-
 
     public DijstraPosition(int realX, int realY,int cost,boolean reachable,int x,int y) {
         this.realX = realX;
